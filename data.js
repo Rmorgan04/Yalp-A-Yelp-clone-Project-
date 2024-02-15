41dde0a2e11049a71b3de5a449acc24341f333ae
Robby Morgan <robby.d.morgan@gmail.com>
2024-02-15 16:38:26 -0600

    added recent activity section and made some styling changes

---
+++ data.js
@@ -0,0 +1,21 @@
+
+
+function toggleBus() {
+    document.getElementById('business-dropdn').classList.toggle('hidden')
+}
+
+function toggleRes() {
+    document.getElementById('restaurant-dropdn').classList.toggle('hidden')
+}
+
+function toggleHome() {
+    document.getElementById('home-dropdn').classList.toggle('hidden')
+}
+
+function toggleAuto() {
+    document.getElementById('auto-dropdn').classList.toggle('hidden')
+}
+
+function toggleMore() {
+    document.getElementById('more-dropdn').classList.toggle('hidden')
+}
