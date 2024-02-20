7df04cbd1ebdeb276de74813b7a3a34d36b9cb65
Robby Morgan <robby.d.morgan@gmail.com>
2024-02-20 12:09:22 -0600

    added hover and click events

--- data.js
+++ data.js
@@ -1,21 +1,20 @@
 
 
-function toggleBus() {
-    document.getElementById('business-dropdn').classList.toggle('hidden')
+function toggleElement(elementId) {
+    const element = document.getElementById(elementId);
+    element.classList.toggle('hidden');
+ 
 }
 
-function toggleRes() {
-    document.getElementById('restaurant-dropdn').classList.toggle('hidden')
+function showMore() {
+    const show = document.getElementById('more-activity-btn');
+    const activitySection = document.getElementById('activity-section');
+
+    activitySection.style.maxHeight = '3000px';
+    activitySection.classList.remove('overflow-hidden');
+    show.classList.add('hidden');
 }
 
-function toggleHome() {
-    document.getElementById('home-dropdn').classList.toggle('hidden')
-}
-
-function toggleAuto() {
-    document.getElementById('auto-dropdn').classList.toggle('hidden')
-}
-
-function toggleMore() {
-    document.getElementById('more-dropdn').classList.toggle('hidden')
-}
+// document.addEventListener('click', (event) => {
+//     if ()
+// })
